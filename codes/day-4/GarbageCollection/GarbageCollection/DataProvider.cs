7821f24b02f36360710d660ca22dd556bef2a867
joydip-git <joydip.mondal@gmail.com>
2025-11-20 11:33:00 +0530

    Gerbage Collection

---
+++ codes/day-4/GarbageCollection/GarbageCollection/DataProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+
+namespace GarbageCollection
+{
+    public class DataProvider : IDisposable
+    {
+        private StreamReader? _reader;
+
+        ~DataProvider()
+        {
+            Console.WriteLine($"destructor being called on thread no: {Environment.CurrentManagedThreadId}...all system resources will be released...");
+            if (_reader != null)
+            {
+                _reader.Close();
+                _reader.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"dispose being called on thread no: {Environment.CurrentManagedThreadId}...all system resources will be released...");
+            if (_reader != null)
+            {
+                _reader.Close();
+                _reader.Dispose();
+            }
+            GC.SuppressFinalize(this);            
+        }
+
+        public string GetData()
+        {
+            _reader = new StreamReader("data.txt");
+            StringBuilder data = new StringBuilder();
+            while (!_reader.EndOfStream)
+            {
+                string? line = _reader.ReadLine();
+                data.AppendLine(line);
+            }
+            return data.ToString();
+        }
+    }
+}
---
+++ codes/day-4/GarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace GarbageCollection
+{
+    internal class Program
+    {
+        static void Swap(out int a)
+        {
+            a = 6;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread No: " + Environment.CurrentManagedThreadId);
+            int x;
+            Swap(out x);
+            Console.WriteLine(x);
+            string data = UseDataProvider();
+            Console.WriteLine(data);
+            //WeakReference<DataProvider> providerRef = UseDataProvider();            
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+            //GC.Collect();
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+            //GC.Collect();
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+
+            //DataProvider? provider;
+            //bool doesExist = providerRef.TryGetTarget(out provider);
+            //if (doesExist)
+            //{
+            //    Console.WriteLine(provider?.GetData());
+            //}
+
+            GC.Collect();
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+            GC.Collect();
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+            GC.Collect();
+            //Console.WriteLine(GC.GetGeneration(providerRef));
+        }
+        //static WeakReference<DataProvider> UseDataProvider()
+        //{
+        //    DataProvider provider = new DataProvider();
+        //    string data = provider.GetData();
+        //    Console.WriteLine(data);
+        //    return new WeakReference<DataProvider>(provider);
+        //}
+        static string UseDataProvider()
+        {
+            string data = string.Empty;
+            using (DataProvider provider = new DataProvider())
+            {
+                data = provider.GetData();
+            }
+            //using DataProvider provider = new DataProvider();
+            //data = provider.GetData();
+
+            return data;
+        }
+    }
+}
